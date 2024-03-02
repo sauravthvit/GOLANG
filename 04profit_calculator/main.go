faac65763b91733a927b171b57e0ca19be1ba3c4
My Name <sauravth2003@gmail.com>
2024-03-02 20:51:20 +0530

    added

---
+++ 04profit_calculator/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main2() {
+
+	var taxRate float64
+	var revenue float64
+	var expenses float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+}
+
+
