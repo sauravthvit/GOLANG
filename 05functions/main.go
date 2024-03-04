e9ac67bdec805b6339cf1a9d93b09674926413e1
My Name <sauravth2003@gmail.com>
2024-03-04 21:09:08 +0530

    added

---
+++ 05functions/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Created Variables
+	// var a = 10
+	// investmentAmount, years := 1000, 5.5
+	const inflationRate = 6.5 //Can't changed thereafter
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5 //Shortcut of Autosetting of variable type
+
+	outputText("Investment Amount: ")
+	//Scans the terminal for input
+	fmt.Scan(&investmentAmount) //Using pointers
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
