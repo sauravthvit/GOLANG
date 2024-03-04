6db11c5eb8eb0fa192b867e6639f8f95d5d3cb45
My Name <sauravth2003@gmail.com>
2024-03-04 21:02:40 +0530

    added

--- 04profit_calculator/main.go
+++ 04profit_calculator/main.go
@@ -26,5 +26,12 @@ func main() {
 	fmt.Println("Profit:", profit)
 	fmt.Println("Ratio:", ratio)
 	fmt.Printf("EBT: %v\nProfit: %v\n", ebt, profit) //You can use printf and multiple placeholders and write the variables in correct order.
-	//You can add other type of placeholders depending on your needs like %f gives you output with shorter decimal places, %.0f gives numbers with no decimal places. 
+	//You can add other type of placeholders depending on your needs like %f gives you output with shorter decimal places, %.0f gives numbers with no decimal places.
+
+	formattedRV := fmt.Sprintf("EBT: %.1f\n", ebt)
+	fmt.Print(formattedRV)
+
+	//For multiline strings use ``
+
+	
 }
